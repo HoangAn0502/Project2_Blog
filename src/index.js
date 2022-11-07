2fa714331adcf3a38bc061d6c11538bf9fd0d1a9
HoangAn52302 <an1052002@gmail.com>
2022-11-07 20:03:38 +0700

    Anhn_upload khung suon code

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require ('express-handlebars');
+
+const app = express();
+const port = 3000
+
+//add file morgan middleware
+app.use(morgan('combined'));
+
+//App file handlebars middleware
+app.engine('handlebars', engine());
+
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname,'./resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
+
